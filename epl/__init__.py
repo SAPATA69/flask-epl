5820edfc5dbe08b06ee1eea2ff7bc8efcbcc3660
SAPATA69 <crca4577@gmail.com>
2026-01-20 20:08:00 +0700

    add

--- epl/__init__.py
+++ epl/__init__.py
@@ -1,16 +1,6 @@
-﻿from flask import Flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 
 app = Flask(__name__)
-
-# สร้างฟลเดอร instance ถ้ายังไม่มี
-instance_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'instance')
-os.makedirs(instance_path, exist_ok=True)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.sqlite3'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.db'
 db = SQLAlchemy(app)
-
-from epl import routes, models
