f18e2d741b4037626da0d43173ef3caa82df9353
Pattharaphon <crca4577@gmail.com>
2026-02-09 21:19:46 +0700

    add

--- epl/__init__.py
+++ epl/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
 from flask_migrate import Migrate
-
-
+import os
 
 app = Flask(__name__)
 
@@ -15,9 +13,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-Migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
+# ลบบรรทัดนี้ออก
+# from epl import routes, models
 
-db = SQLAlchemy(app)
-
-from epl import routes, models
